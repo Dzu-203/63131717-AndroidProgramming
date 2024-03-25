2413d314e220b91c66126aba3544f448758ba3ae
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-03-25 19:03:18 +0700

    update
    update

--- ntu.63131717/Cau3_Introduce/app/src/main/java/com/example/cau3_introduce/MainActivity.java
+++ ntu.63131717/Cau3_Introduce/app/src/main/java/com/example/cau3_introduce/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.cau3_introduce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton fb,ins,git;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addInit();
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/profile.php?id=100073235486899"));
+                startActivity(intent);
+            }
+        });
+        ins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/"));
+                startActivity(intent);
+            }
+        });
+        git.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Dzu-203"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void addInit() {
+        fb = findViewById(R.id.btnfb);
+        ins = findViewById(R.id.btnig);
+        git = findViewById(R.id.btngit);
     }
 }
