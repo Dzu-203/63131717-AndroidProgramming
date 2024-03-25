8f8dbe3c1c5e8df0ab4e03964efd6dcbf0b9d05a
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-03-25 19:04:03 +0700

    update
    update

--- ntu.63131717/Cau3_Introduce/app/src/main/java/com/example/cau3_introduce/MainActivity.java
+++ ntu.63131717/Cau3_Introduce/app/src/main/java/com/example/cau3_introduce/MainActivity.java
@@ -24,7 +24,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+        ins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/"));
+                startActivity(intent);
+            }
+        });
         git.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
