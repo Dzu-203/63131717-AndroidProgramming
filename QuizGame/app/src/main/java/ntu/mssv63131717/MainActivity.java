f25ec56ae377d56c8ab0af609617d661a6334a30
Dzu_203 <108659135+Dzu-203@users.noreply.github.com>
2024-05-12 20:10:58 +0700

    tạo giao diện menu

--- QuizGame/app/src/main/java/ntu/mssv63131717/MainActivity.java
+++ QuizGame/app/src/main/java/ntu/mssv63131717/MainActivity.java
@@ -1,19 +1,39 @@
 package ntu.mssv63131717;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
+
+import ntu.mssv63131717.Activities.LevelActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView html,js,py,android;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        GetInit();
+        SetUpOnclick();
     }
+
+    private void GetInit() {
+        html = findViewById(R.id.html);
+        js = findViewById(R.id.js);
+        py = findViewById(R.id.py);
+        android = findViewById(R.id.android);
+    }
+    private void SetUpOnclick(){
+        View.OnClickListener listener = v -> {
+            Intent intent = new Intent(MainActivity.this, LevelActivity.class);
+            startActivity(intent);
+        };
+        html.setOnClickListener(listener);
+        js.setOnClickListener(listener);
+        py.setOnClickListener(listener);
+        android.setOnClickListener(listener);
+    }
+
 }
